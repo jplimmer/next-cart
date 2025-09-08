f095b55abebffa62c98c10823a3594a02f9d0777
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-08 15:34:51 +0200

    Created constant with navigation routes

---
+++ src/lib/constants.ts
@@ -0,0 +1,18 @@
+export const ROUTES = {
+  HOME: {
+    href: '/',
+    label: 'Home',
+  },
+  ABOUT: {
+    href: '/about',
+    label: 'About us',
+  },
+  CONTACT: {
+    href: '/contact',
+    label: 'Contact',
+  },
+  PRODUCTS: {
+    href: '/products',
+    label: 'Products',
+  },
+} as const;
