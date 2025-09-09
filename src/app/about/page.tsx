9a418782efbe08726f7fc89afe8b6d40683d52d3
Tob-private <tobias.riiser.games@gmail.com>
2025-09-09 15:08:19 +0200

    added some basic data on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,56 @@
 export default function about() {
-  return <div>Content</div>;
+  return (
+    <main>
+      <section className="text-white bg-[#004F44] p-20">
+        <h1 className="text-5xl w-1/3 mb-12">
+          NextCart is more than an e-commerce website
+        </h1>
+        <p>Our story begins in a galaxy, far, far away</p>
+      </section>
+      <section className="flex w-3/4 m-auto p-10 gap-8 flex-col">
+        <article className="max-w-1/3">
+          <h2 className="text-3xl">Story</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            repellat repellendus atque maiores! Beatae culpa hic accusamus totam
+            quis distinctio harum rerum voluptate atque consequatur aliquam odit
+            dicta, sit necessitatibus eaque corrupti quo, dolor tempore nam
+            quibusdam omnis suscipit eum accusantium a. Modi porro odit iusto
+            voluptatibus nisi. Voluptas facilis molestias officia aperiam, hic,
+            rem soluta, labore repellendus deleniti quibusdam nobis accusamus
+            sunt. Qui dolore inventore illum quia enim atque esse beatae sequi
+            aperiam est!
+          </p>
+        </article>
+        <article className="max-w-1/3 self-end">
+          <h2 className="text-3xl">Goals</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            repellat repellendus atque maiores! Beatae culpa hic accusamus totam
+            quis distinctio harum rerum voluptate atque consequatur aliquam odit
+            dicta, sit necessitatibus eaque corrupti quo, dolor tempore nam
+            quibusdam omnis suscipit eum accusantium a. Modi porro odit iusto
+            voluptatibus nisi. Voluptas facilis molestias officia aperiam, hic,
+            rem soluta, labore repellendus deleniti quibusdam nobis accusamus
+            sunt. Qui dolore inventore illum quia enim atque esse beatae sequi
+            aperiam est!
+          </p>
+        </article>
+        <article className="max-w-1/3">
+          <h2 className="text-3xl">Environment</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla
+            repellat repellendus atque maiores! Beatae culpa hic accusamus totam
+            quis distinctio harum rerum voluptate atque consequatur aliquam odit
+            dicta, sit necessitatibus eaque corrupti quo, dolor tempore nam
+            quibusdam omnis suscipit eum accusantium a. Modi porro odit iusto
+            voluptatibus nisi. Voluptas facilis molestias officia aperiam, hic,
+            rem soluta, labore repellendus deleniti quibusdam nobis accusamus
+            sunt. Qui dolore inventore illum quia enim atque esse beatae sequi
+            aperiam est!
+          </p>
+        </article>
+      </section>
+    </main>
+  );
 }
