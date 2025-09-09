871d0daff42bfbda61a5749e314f281d280a2a8d
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-09 12:17:38 +0200

    Wrapped header logo in Link to Home page

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,7 @@
 import { ASSET_PATHS } from '@/lib/constants/assets';
+import { routes } from '@/lib/constants/routes';
 import Image from 'next/image';
+import Link from 'next/link';
 import { PagesNav } from './pages-nav';
 import { UserNav } from './user-nav';
 
@@ -7,10 +9,10 @@ export function Header() {
   return (
     <div className="flex justify-between mt-4">
       <PagesNav />
-      <div className="flex items-center gap-2">
+      <Link href={routes.home.href} className="flex items-center gap-2">
         <span className="font-bold">NextCart</span>
         <Image src={ASSET_PATHS.LOGO} alt="" width={32} height={32} />
-      </div>
+      </Link>
       <UserNav />
     </div>
   );
