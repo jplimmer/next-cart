d7cb1ad181fa53657bfbcd48e667f94a29bdf418
Tob-private <tobias.riiser.games@gmail.com>
2025-09-09 13:52:26 +0200

    Reduced opacity slightly

--- src/components/root-page/hero-img-text.tsx
+++ src/components/root-page/hero-img-text.tsx
@@ -1,6 +1,6 @@
 export default function HeroImgText({ text }: { text: string }) {
   return (
-    <span className="bg-green-950/80 p-3 z-10 absolute left-1 top-1">
+    <span className="bg-green-950/75 p-3 z-10 absolute left-1 top-1">
       {text}
     </span>
   );
