4c565818d34687c8acdfbd65f7f58abfc11c21f5
Tommy <Tommy@DESKTOP-UJVT1RG>
2025-09-09 15:06:56 +0200

    make use of logo

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,4 +1,7 @@
 import { Button } from '@/components/ui/button';
+import { ASSET_PATHS } from '@/lib/constants/assets';
+import { routes } from '@/lib/constants/routes';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Footer() {
@@ -6,9 +9,10 @@ export default function Footer() {
     <footer className="bg-[#004F44] text-white py-10 px-6">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
         {/* Logo */}
-        <div>
-          <h2 className="text-2xl font-bold tracking-wide">NextCart</h2>
-        </div>
+        <Link href={routes.home.href} className="flex items-center gap-2">
+          <span className="font-bold">NextCart</span>
+          <Image src={ASSET_PATHS.LOGOWHITE} alt="" width={32} height={32} />
+        </Link>
 
         {/* Social Icons */}
         <div className="text-center">
