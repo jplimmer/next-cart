536861e54c814c737969b203d5bafcc7a5472941
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-09 14:02:53 +0200

    Remove main tag from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${iconsSocialMedia15Colr.variable} antialiased`}
       >
         <Header />
-        <main>{children}</main>
+        {children}
         <Footer />
       </body>
     </html>
