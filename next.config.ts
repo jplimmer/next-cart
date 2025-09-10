6d5f294dc3dd769a99d9d3a52f9ca87b6767414f
Arif.A <arif.sain@gmail.com>
2025-09-10 14:00:35 +0200

    Configure image domains in nextJs config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'api.lorem.space',
+      'placeimg.com',
+      'i.imgur.com',
+      'picsum.photos',
+      'api.escuelajs.co',
+    ],
+  },
 };
 
 export default nextConfig;
