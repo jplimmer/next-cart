10f9b45be4f5d3485a8e4914a127bd065f9e0419
Tob-private <tobias.riiser.games@gmail.com>
2025-09-11 14:38:41 +0200

    created product card flexbox

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,3 +1,19 @@
-export default function Products() {
-  return <div>Content</div>;
+import ProductCard from '@/components/product-card';
+import { getProducts } from '@/lib/api/products-data-server';
+import { Product } from '@/lib/types/product';
+
+export default async function Products() {
+  const products = await getProducts();
+  return (
+    <main>
+      <h1 className="text-4xl text-center my-16">Products</h1>
+      <section className="flex flex-wrap gap-6 w-5/6 m-auto mb-32 justify-center">
+        {products?.map((product: Product) => (
+          <section className="w-1/4" key={product.id}>
+            <ProductCard product={product} key={product.id} />
+          </section>
+        ))}
+      </section>
+    </main>
+  );
 }
