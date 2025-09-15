083bd714a8554de0e9751e888d3192de4d9e7d79
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-15 17:06:19 +0200

    Add string utils for capitalisation and query-split

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function capitaliseFirstLetter(string: string) {
+  return String(string).charAt(0).toUpperCase() + String(string).slice(1);
+}
+
+export function splitByQuery(result: string, query: string) {
+  const parts = result.split(query);
+  return {
+    before: parts[0],
+    query: query,
+    after: parts.slice(1).join(query),
+  };
+}
+
 const toRgb = converter('rgb');
 
 export function resolveTailwindColorToHex(
