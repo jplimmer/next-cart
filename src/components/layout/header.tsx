848cf1b99fe948a0c37dad432bc90ac0bd22f275
Tommy <Tommy@DESKTOP-UJVT1RG>
2025-09-16 11:08:46 +0200

    sticky nav proposal

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,7 +6,7 @@ import { UtilitiesNav } from './utilities-nav';
 
 export function Header() {
   return (
-    <header className="grid grid-cols-3 items-center justify-items-center my-2">
+    <header className="sticky top-0 z-[1000] bg-white/93 p-2 grid grid-cols-3 items-center justify-items-center my-2">
       <PagesNav className="justify-self-start" />
       <Link href={routes.home.href} className="flex items-center gap-2">
         <span className="font-bold">NextCart</span>
