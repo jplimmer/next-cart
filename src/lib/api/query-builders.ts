da9cfd112dce32e868e40b773c741d313331c358
Tob-private <tobias.riiser.games@gmail.com>
2025-09-18 14:51:06 +0200

    removed unused query

--- src/lib/api/query-builders.ts
+++ src/lib/api/query-builders.ts
@@ -3,33 +3,6 @@ import { QueryFilters } from '../types/types';
 // These query builders are used when we need to mimic the behavior that "where:" usually enables
 // Since we cant pass arrays into their graphQL filters, we have to generate an alias for each filter that
 
-export function buildCompleteProductsQueryByIDs(productIDs: number[]) {
-  return `
-    query {
-      ${productIDs
-        .map(
-          (id, i) => `
-        Product_${i}: product(id: ${id}) {
-           id
-            title
-            slug
-            price
-            description
-            images
-            category {
-                id
-                name
-                slug
-                image
-            }
-        }
-        `
-        )
-        .join('\n')}
-    }
-  `;
-}
-
 export function buildProductsQueryByFilters(filters: QueryFilters) {
   const { categoryIDs = [], title = '' } = filters;
 
