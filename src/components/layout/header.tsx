d527a748a87b3c758e435559b241903bc61f9d2b
Tommy <Tommy@DESKTOP-UJVT1RG>
2025-09-18 10:55:51 +0200

    disabled a div

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
     <>
       <div className="absolute inset-0 full-width bg-gradient-to-tr from-green-950 to-green-800 h-[52]"></div>
       <header className={`full-width sticky top-0 z-[1000] py-2 bg-black/50`}>
-        <div className="absolute inset-0 full-width bg-gradient-to-tr from-green-950/20 to-green-800/20 h-[52]"></div>
+        {/* <div className="absolute inset-0 full-width bg-gradient-to-tr from-green-950/20 to-green-800/20 h-[52]"></div> */}
         <div className="grid grid-cols-3 items-center justify-items-center">
           <PagesNav className="justify-self-start" />
           <Link href={routes.home.href} className="flex items-center gap-2">
