57367882084db0637493b14dac803f2872c12ae2
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-19 09:59:24 +0200

    Footer layout tweak

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -6,9 +6,12 @@ import { Logo } from './logo';
 export default function Footer() {
   return (
     <footer className="full-width bg-[#004F44] text-white py-10">
-      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8 items-start">
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-8 items-start px-4">
         {/* Logo */}
-        <Link href={routes.home.href} className="flex items-center gap-2">
+        <Link
+          href={routes.home.href}
+          className="flex items-center justify-center md:justify-start gap-2"
+        >
           <span className="font-bold">NextCart</span>
           <Logo size={32} colour="white" />
         </Link>
@@ -39,11 +42,11 @@ export default function Footer() {
         </div>
 
         {/* Newsletter CTA */}
-        <div className="text-right">
+        <div className="text-center md:text-right">
           <p className="mb-4">
             Receive the Latest News and Exclusive Offers by
           </p>
-          <div className="flex justify-end space-x-2">
+          <div className="flex justify-center md:justify-end space-x-2">
             <Button variant="outline" className="bg-white text-[#004F44]">
               TEXT
             </Button>
@@ -58,8 +61,10 @@ export default function Footer() {
       <div className="border-t border-white my-6"></div>
 
       {/* Bottom Section */}
-      <div className="grid grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-4 items-center text-sm">
-        <p>© 2025 NextCart - order like there´s no tomorrow</p>
+      <div className="grid grid-rows-3 md:grid-cols-3 md:grid-rows-1 gap-4 items-center text-sm px-4">
+        <p className="text-center md:text-left">
+          © 2025 NextCart - order like there´s no tomorrow
+        </p>
         <div className="flex justify-center space-x-4">
           <Link href="#" className="underline">
             Terms of Use
@@ -71,7 +76,7 @@ export default function Footer() {
         <div className="text-center md:text-right">
           <p>777 Casino Ave. Thackerville, OK 73459</p>
           <p>1-800-622-6317</p>
-        </div>{' '}
+        </div>
       </div>
     </footer>
   );
