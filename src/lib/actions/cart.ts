0ad7cdcfee8d495bda33f0bfb56b525b05e9e121
Tob-private <tobias.riiser.games@gmail.com>
2025-09-19 09:16:57 +0200

    lint

--- src/lib/actions/cart.ts
+++ src/lib/actions/cart.ts
@@ -1,7 +1,6 @@
 'use server';
 
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 
 export async function addToCartAction() {
   const cookieStore = await cookies();
