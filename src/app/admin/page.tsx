8dc98fc331d2ec29da978346164b5bf142a22c2d
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-22 20:00:49 +0200

    Add basic product fetching logic to AdminPage

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,34 @@
 import ProductForm from '@/components/admin/product-form';
-import ProductTable from '@/components/admin/products-table';
+import ProductsTable from '@/components/admin/products-table';
 import ToggleForm from '@/components/admin/toggle-form-button';
+import {
+  getProductsByIds,
+  getProductsLight,
+} from '@/lib/data/product-data-service';
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const allProducts = await getProductsLight();
+
+  if (!(allProducts.length > 0)) {
+    return <p className="italic">No products found</p>;
+  }
+
+  const MAX_PER_PAGE = 20;
+  const firstPageProducts = allProducts.slice(0, MAX_PER_PAGE);
+  const productsResult = await getProductsByIds(
+    firstPageProducts.map((p) => p.id)
+  );
+
+  if (!productsResult.success) {
+    return <p className="italic">No products found</p>;
+  }
+
   return (
     <main className="content-grid full-width py-8">
       <ToggleForm>
         <ProductForm />
       </ToggleForm>
-      <ProductTable />
+      <ProductsTable />
     </main>
   );
 }
