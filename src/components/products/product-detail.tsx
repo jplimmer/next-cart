a81e48846b410b965ac7dd9ef97fd37d5108694b
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-22 11:35:41 +0200

    Extract ProductDetail into separate component, with ImageCarousel and semantic improvements

---
+++ src/components/products/product-detail.tsx
@@ -0,0 +1,34 @@
+import { Product } from '@/lib/types/product';
+import QuantitySelector from '../quantity-selector';
+import { Button } from '../ui/button';
+import ProductImageCarousel from './product-image-carousel';
+
+export default function ProductDetail({ product }: { product: Product }) {
+  return (
+    <article className="w-full flex justify-center items-center gap-16">
+      <section className="flex-1">
+        <h1
+          id={`product-${product.id}-header`}
+          className="mb-2 text-2xl font-bold"
+        >
+          {product.title}
+        </h1>
+        <p className="my-2 text-lg font-semibold">${product.price}</p>
+        <p id={`product-${product.id}-description`} className="text-justify">
+          {product.description}
+        </p>
+
+        <section className="flex items-baseline justify-between w-full">
+          <QuantitySelector className="my-3" />
+          <span className="font-semibold text-xs bg-secondary text-secondary-foreground px-2 py-1 rounded-full">
+            {product.category.name}
+          </span>
+        </section>
+        <Button className="my-2 w-full">Add to Cart</Button>
+      </section>
+      <div className="flex-1 aspect-square w-full relative overflow-hidden rounded-md order-first">
+        <ProductImageCarousel images={product.images} title={product.title} />
+      </div>
+    </article>
+  );
+}
