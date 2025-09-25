6ecd22ecc6989cf8c8cbe6f9c5e7414ab69fb0cb
Tob-private <tobias.riiser.games@gmail.com>
2025-09-25 10:37:25 +0200

    added update schema and updated types to accommodate for it

--- src/lib/schemas/product-form.ts
+++ src/lib/schemas/product-form.ts
@@ -1,17 +1,18 @@
 import z from 'zod';
 
 export type CreateProductFormData = z.infer<typeof createSchema>;
+export type UpdateProductFormData = z.infer<typeof updateSchema>;
 
-export type CreateProductFormErrors = Partial<
-  Record<keyof CreateProductFormData, string[]>
+export type ProductFormErrors = Partial<
+  Record<keyof CreateProductFormData | keyof UpdateProductFormData, string[]>
 >;
 
 export type CreateProductFormState =
-  | { success: true; data: CreateProductFormData }
+  | { success: true; data: CreateProductFormData | UpdateProductFormData }
   | {
       success: false;
-      error: CreateProductFormErrors;
-      data: CreateProductFormData;
+      error: ProductFormErrors;
+      data: CreateProductFormData | UpdateProductFormData;
     };
 
 export const createSchema = z.object({
@@ -41,4 +42,35 @@ export const createSchema = z.object({
         'Your image URL isnt safe, make sure it starts with "https://"'
       )
   ),
+});
+export const updateSchema = z.object({
+  title: z
+    .string()
+    .min(
+      3,
+      'Your title is too short. Make sure it is at least 3 characters long'
+    )
+    .optional(),
+  description: z
+    .string()
+    .min(
+      10,
+      'Your description is too short. Make sure it is at least 10 characters long'
+    )
+    .optional(),
+  price: z.coerce
+    .number()
+    .positive('Your price is a negative number. Make sure its a postive number')
+    .optional(),
+  categoryID: z.coerce.number().optional(),
+  images: z
+    .array(
+      z
+        .string()
+        .startsWith(
+          'https://',
+          'Your image URL isnt safe, make sure it starts with "https://"'
+        )
+    )
+    .optional(),
 });
