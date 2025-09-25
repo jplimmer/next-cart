66e0d838eac72f015b6b254baf01aaac48d1a957
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-25 14:44:39 +0200

    Changed UserNavItem to use clerk's UserButton

--- src/components/navigation/basket-nav-item.tsx
+++ src/components/navigation/basket-nav-item.tsx
@@ -18,7 +18,7 @@ export function BasketNavItem({
   return (
     <NavigationMenuItem>
       <NavigationMenuTrigger className="[&>svg:last-child]:hidden relative">
-        <ShoppingCart />
+        <ShoppingCart className="cursor-pointer" />
         {cartCount > 0 && (
           <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
             {cartCount > 99 ? '99+' : cartCount}
--- src/components/navigation/user-nav-item.tsx
+++ src/components/navigation/user-nav-item.tsx
@@ -1,75 +1,49 @@
+'use client';
+
 import { routes } from '@/lib/constants/routes';
 import {
+  ClerkLoaded,
+  ClerkLoading,
   SignedIn,
   SignedOut,
   SignInButton,
-  SignOutButton,
+  UserButton,
 } from '@clerk/nextjs';
-import { LogOut, UserRound } from 'lucide-react';
-import Link from 'next/link';
-import {
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuTrigger,
-} from '../ui/navigation-menu';
-
-const linkStyle =
-  "data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus-visible:ring-ring/50 [&amp;_svg:not([class*='text-'])]:text-muted-foreground gap-1 rounded-sm p-2 text-sm transition-all outline-none focus-visible:ring-[3px] focus-visible:outline-1 [&amp;_svg:not([class*='size-'])]:size-4";
+import { NotebookPen, UserRound } from 'lucide-react';
+import { navigationMenuTriggerStyle } from '../ui/navigation-menu';
 
 export function UserNavItem() {
   return (
-    <NavigationMenuItem>
-      <NavigationMenuTrigger className="[&>svg:last-child]:hidden">
-        <UserRound />
-        <span className="sr-only">Account</span>
-      </NavigationMenuTrigger>
-      <NavigationMenuContent>
+    <div className="flex items-center">
+      <ClerkLoading>
+        <div className={navigationMenuTriggerStyle()}>
+          <UserRound size={28} color="grey" className="animate-pulse" />
+        </div>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <div className={navigationMenuTriggerStyle()}>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Link
+                  label="Manage catalog"
+                  labelIcon={<NotebookPen size={16} />}
+                  href={routes.admin.href}
+                />
+                <UserButton.Action label="manageAccount" />
+                <UserButton.Action label="signOut" />
+              </UserButton.MenuItems>
+            </UserButton>
+          </div>
+        </SignedIn>
         <SignedOut>
           <SignInButton>
-            <span
-              className={`flex flex-row items-center whitespace-nowrap ${linkStyle}`}
-            >
-              Sign in
-            </span>
+            <div className={navigationMenuTriggerStyle()}>
+              <UserRound size={28} className="cursor-pointer" />
+            </div>
           </SignInButton>
         </SignedOut>
-        <SignedIn>
-          <ul>
-            <li>
-              <NavigationMenuLink asChild>
-                <Link href={routes.admin.href} className="whitespace-nowrap">
-                  {routes.admin.title}
-                </Link>
-              </NavigationMenuLink>
-            </li>
-            <li>
-              <NavigationMenuLink asChild>
-                <Link href="#" className="whitespace-nowrap">
-                  Account settings
-                </Link>
-              </NavigationMenuLink>
-            </li>
-            <li>
-              <NavigationMenuLink asChild>
-                <Link href="#">Messages</Link>
-              </NavigationMenuLink>
-            </li>
-            <li>
-              <SignOutButton>
-                <NavigationMenuLink asChild>
-                  <span
-                    className={`flex flex-row items-center whitespace-nowrap ${linkStyle}`}
-                  >
-                    Log out
-                    <LogOut />
-                  </span>
-                </NavigationMenuLink>
-              </SignOutButton>
-            </li>
-          </ul>
-        </SignedIn>
-      </NavigationMenuContent>
-    </NavigationMenuItem>
+      </ClerkLoaded>
+    </div>
   );
 }
