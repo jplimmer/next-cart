9d78e10adbf0d8f64bd0322e9a080b29ea8f2c8a
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-26 11:25:15 +0200

    Wrap admin tabs in basic suspense boundaries

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,8 @@
 import {
+  categoryColumns,
+  CategoryTableEntry,
+} from '@/components/admin/category-colums';
+import {
   productColumns,
   ProductTableEntry,
 } from '@/components/admin/product-columns';
@@ -8,6 +12,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { routes } from '@/lib/constants/routes';
 import { getProducts } from '@/lib/data/product-data-service';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 const getProductTableEntries = async (): Promise<ProductTableEntry[]> => {
   const allProducts = await getProducts();
@@ -50,8 +55,6 @@ const getCategoryTableEntries = async (): Promise<CategoryTableEntry[]> => {
 };
 
 export default async function AdminPage() {
-  const products = await getProductTableEntries();
-
   return (
     <main className="full-width p-8 space-y-8">
       <div className="flex flex-col">
@@ -66,29 +69,69 @@ export default async function AdminPage() {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="products" className="flex flex-col p-4 space-y-3">
-            <DataTable
-              columns={productColumns}
-              data={products}
-              filterColumn="title"
-              filterPlaceholder="Filter by product name..."
-              addNewButton={
-                <Button asChild className="self-end bg-green-900">
-                  <Link href={routes.createProduct.href}>Add new product</Link>
-                </Button>
+            <Suspense
+              fallback={
+                <div className="flex w-full justify-center items-center mt-12 gap-4">
+                  Loading...
+                  <div className="h-4 w-4 animate-spin rounded-full border-2 border-indigo-300 border-t-transparent"></div>
+                </div>
               }
-            />
+            >
+              <ProductsTab />
+            </Suspense>
           </TabsContent>
           <TabsContent
             value="categories"
             className="flex flex-col p-4 space-y-3"
           >
-            <Button asChild className="self-end bg-green-900">
-              <Link href={routes.createCategory.href}>Add new category</Link>
-            </Button>
-            <p>Categories table to go here</p>
+            <Suspense
+              fallback={
+                <div className="flex w-full justify-center items-center mt-12">
+                  Loading...
+                </div>
+              }
+            >
+              <CategoriesTab />
+            </Suspense>
           </TabsContent>
         </Tabs>
       </div>
     </main>
   );
 }
+
+const ProductsTab = async () => {
+  const products = await getProductTableEntries();
+
+  return (
+    <DataTable
+      columns={productColumns}
+      data={products}
+      filterColumn="title"
+      filterPlaceholder="Filter by product name..."
+      addNewButton={
+        <Button asChild className="self-end bg-green-900">
+          <Link href={routes.createProduct.href}>Add new product</Link>
+        </Button>
+      }
+    />
+  );
+};
+
+const CategoriesTab = async () => {
+  const categories = await getCategoryTableEntries();
+
+  return (
+    <DataTable
+      columns={categoryColumns}
+      data={categories}
+      filterColumn="name"
+      filterPlaceholder="Filter by category name..."
+      addNewButton={
+        <Button asChild className="self-end bg-green-900">
+          <Link href={routes.createCategory.href}>Add new category</Link>
+        </Button>
+      }
+    />
+  );
+};
