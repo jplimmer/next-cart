bda8e372f6274f5015a3149ed791930ce7ba63e2
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-29 14:38:50 +0200

    Add new products section with separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import FeaturedProducts from '@/components/root-page/featured-products';
 import Hero from '@/components/root-page/hero';
+import { NewProducts } from '@/components/root-page/new-products';
+import { Separator } from '@/components/ui/separator';
 
-export default async function Home() {
+export default function Home() {
   return (
-    <main className="content-grid full-width">
+    <main className="content-grid full-width space-y-8">
       <Hero />
       <FeaturedProducts />
+      <Separator />
+      <NewProducts />
     </main>
   );
 }
---
+++ src/components/root-page/new-products.tsx
@@ -0,0 +1,21 @@
+import { getProductsPaginated } from '@/lib/data/product-data-service';
+import { Suspense } from 'react';
+import { CardGrid, CardGridLayout } from '../products/card-grid';
+
+export function NewProducts() {
+  const numProducts = 4;
+  const offset = 4;
+
+  return (
+    <section className="content-grid full-width bg-gray-50 space-y-8 py-8">
+      <header className={CardGridLayout}>
+        <h2 className=" col-span-full text-green-800 text-3xl text-center md:text-left">
+          New arrivals
+        </h2>
+      </header>
+      <Suspense fallback={''}>
+        <CardGrid productsPromise={getProductsPaginated(numProducts, offset)} />
+      </Suspense>
+    </section>
+  );
+}
