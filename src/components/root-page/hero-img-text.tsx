b130a1dc556363d0f709b58e3f157c9d589306ee
Tommy <Tommy@DESKTOP-UJVT1RG>
2025-09-29 12:06:09 +0200

    Add actual products to hero, and a git ignore that gets auto created regarded clerc

--- src/components/root-page/hero-img-text.tsx
+++ src/components/root-page/hero-img-text.tsx
@@ -1,5 +1,13 @@
-export default function HeroImgText({ text }: { text: string }) {
+export default function HeroImgText({
+  text,
+  className,
+}: {
+  text: string;
+  className?: string;
+}) {
   return (
-    <span className="bg-green-950/75 p-3 absolute left-1 top-1">{text}</span>
+    <span className={`bg-green-950/75 p-3 absolute left-1 top-1 ${className}`}>
+      {text}
+    </span>
   );
 }
--- src/components/root-page/hero.tsx
+++ src/components/root-page/hero.tsx
@@ -1,9 +1,59 @@
 import { Button } from '@/components/ui/button';
+import { getSlugFromTitle } from '@/lib/data/helpers';
+import { getProducts } from '@/lib/data/product-data-service';
+import { Product } from '@/lib/types/product';
 import Image from 'next/image';
 import Link from 'next/link';
 import HeroImgText from './hero-img-text';
 
-export default function Hero() {
+export default async function Hero() {
+  const productsFromApi = (await getProducts())?.slice(0, 20); // get 20 first products from api
+  const rndProducts =
+    productsFromApi.length > 3
+      ? Array.from(
+          { length: 3 },
+          () =>
+            productsFromApi.splice(
+              Math.floor(Math.random() * productsFromApi.length),
+              1
+            )[0]
+        )
+      : productsFromApi;
+
+  const generateProducts = (product: Product) => {
+    let txt =
+      product.title?.split(' ')?.filter(Boolean)?.slice(-2)?.join(' ') ||
+      product.title;
+    txt = txt?.toLowerCase().startsWith('and ') ? product.title : txt;
+    return (
+      <Link
+        key={product.id}
+        href={`/products/${getSlugFromTitle(product.title)}`}
+        className="bg-purple-200/50 p-9 flex-1/3 relative"
+      >
+        <HeroImgText
+          text={txt}
+          className={
+            product?.title?.length > 35 ? 'truncate w-[10.625rem]' : ''
+          }
+        />
+        {product?.images.length > 0 ? (
+          <Image
+            src={product.images[0]}
+            alt={product.description}
+            width={150}
+            height={150}
+            className="size-full"
+          />
+        ) : (
+          <div className="flex items-center justify-center bg-green-900/60 text-sm size-full">
+            No image ☹️
+          </div>
+        )}
+      </Link>
+    );
+  };
+
   return (
     <section className="full-width bg-gradient-to-tr from-green-950 to-green-800 text-white">
       <div className="flex items-center justify-around py-16">
@@ -15,36 +65,7 @@ export default function Hero() {
           </Button>
         </div>
         <section className="flex flex-wrap gap-2 w-1/3">
-          <Link href={'/'} className="bg-purple-200/50 p-9 flex-1/3 relative">
-            <HeroImgText text="Popular Laptop" />
-            <Image
-              src={'/hero/laptop.png'}
-              alt="A Popular Laptop made by ROG(Rebublic of Gaming)"
-              width={150}
-              height={150}
-              className="size-full"
-            />
-          </Link>
-          <Link href={'/'} className="bg-purple-200/50 p-9 flex-1/3 relative">
-            <HeroImgText text="New T-Shirt" />
-            <Image
-              src={'/hero/tshirt.png'}
-              alt="A New Blue T-Shirt"
-              width={150}
-              height={150}
-              className="size-full"
-            />
-          </Link>
-          <Link href={'/'} className="bg-purple-200/50 p-9 flex-1/3 relative">
-            <HeroImgText text="Featured Couch" />
-            <Image
-              src={'/hero/couch.png'}
-              alt="A Featured Couch, made in gray cotton"
-              width={150}
-              height={150}
-              className="size-full"
-            />
-          </Link>
+          {rndProducts.map((product) => generateProducts(product))}
         </section>
       </div>
     </section>
