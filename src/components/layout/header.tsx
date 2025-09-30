e203cdea86b25899b35b3b7ac1fd7b83a6fc5510
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-30 11:31:03 +0200

    Updated SidebarTrigger to accept icon as props

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,12 +1,25 @@
 import { routes } from '@/lib/constants/routes';
+import { Menu } from 'lucide-react';
 import Link from 'next/link';
 import { PagesNavMenu, UtilitiesNavMenu } from '../navigation';
+import { SidebarTrigger } from '../ui/sidebar';
 import { Logo } from './logo';
 
 export function Header() {
   return (
     <header className={`full-width my-2`}>
-      <div className="grid grid-cols-3 items-center justify-items-center">
+      {/* Mobile menu */}
+      <div className="md:hidden w-full flex justify-between">
+        {/* <HamburgerNavMenu /> */}
+        <SidebarTrigger icon={<Menu />} />
+        <Link href={routes.home.href} className="flex items-center gap-2">
+          <span className="font-bold">NextCart</span>
+          <Logo size={32} />
+        </Link>
+      </div>
+
+      {/* Desktop menu */}
+      <div className="hidden md:grid grid-cols-3 items-center justify-items-center">
         <PagesNavMenu className="justify-self-start" />
         <Link href={routes.home.href} className="flex items-center gap-2">
           <span className="font-bold">NextCart</span>
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,12 +1,10 @@
 'use client';
 
-import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, VariantProps } from 'class-variance-authority';
 import { PanelLeftIcon } from 'lucide-react';
+import * as React from 'react';
 
-import { useIsMobile } from '@/hooks/use-mobile';
-import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
@@ -24,6 +22,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { cn } from '@/lib/utils';
 
 const SIDEBAR_COOKIE_NAME = 'sidebar_state';
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
@@ -253,11 +253,16 @@ function Sidebar({
   );
 }
 
+interface SidebarTriggerProps extends React.ComponentProps<typeof Button> {
+  icon?: React.ReactNode;
+}
+
 function SidebarTrigger({
   className,
   onClick,
+  icon,
   ...props
-}: React.ComponentProps<typeof Button>) {
+}: SidebarTriggerProps) {
   const { toggleSidebar } = useSidebar();
 
   return (
@@ -273,7 +278,7 @@ function SidebarTrigger({
       }}
       {...props}
     >
-      <PanelLeftIcon />
+      {icon || <PanelLeftIcon />}
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   );
