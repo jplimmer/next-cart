5eb1b9ab7b0b596795eaa1ea397036bca1e07634
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-30 16:47:10 +0200

    Create SearchNavItem with CommandDialog components and add to UtilitiesNav

---
+++ src/components/navigation/search-nav-item.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { routes } from '@/lib/constants/routes';
+import { getSlugFromTitle } from '@/lib/data/helpers';
+import { Product } from '@/lib/types/product';
+import { splitByQuery } from '@/lib/utils';
+import { CommandEmpty } from 'cmdk';
+import { Search } from 'lucide-react';
+import { Suspense, use, useEffect, useState } from 'react';
+import { LoadingSpinner } from '../loading/loading-spinner';
+import { Button } from '../ui/button';
+import {
+  CommandDialog,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '../ui/command';
+
+export function SearchNavItem({
+  productsPromise,
+}: {
+  productsPromise: Promise<Product[]>;
+}) {
+  const [open, setOpen] = useState(false);
+  const [query, setQuery] = useState('');
+
+  // Focuses on search input with Ctrl+K keyboard shorcut
+  useEffect(() => {
+    function handleKeyDown(e: KeyboardEvent) {
+      if ((e.ctrlKey || e.metaKey) && e.key === 'k') {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    }
+    document.addEventListener('keydown', handleKeyDown);
+    return () => document.removeEventListener('keydown', handleKeyDown);
+  }, []);
+
+  return (
+    <>
+      <Button
+        onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+          setOpen((open) => !open);
+        }}
+        variant="ghost"
+        className="flex h-9 items-center gap-6 px-3 rounded-lg border text-muted-foreground font-normal"
+      >
+        <div className="hidden md:flex gap-1">
+          <kbd className="inline-flex items-end border-1 rounded-sm bg-white px-1 text-xs">
+            Ctrl
+          </kbd>
+          <kbd className="inline-flex items-end border-1 rounded-sm bg-white px-1 text-xs">
+            K
+          </kbd>
+          <span>to search...</span>
+        </div>
+        <Search className="size-4 shrink-0 opacity-50" />
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput
+          placeholder="Ctrl+K to search..."
+          value={query}
+          onValueChange={setQuery}
+        />
+        <Suspense fallback={<LoadingSpinner text="Fetching products..." />}>
+          <SearchResults productsPromise={productsPromise} query={query} />
+        </Suspense>
+      </CommandDialog>
+    </>
+  );
+}
+
+function SearchResults({
+  productsPromise,
+  query,
+}: {
+  productsPromise: Promise<Product[]>;
+  query: string;
+}) {
+  const products = use(productsPromise);
+
+  const matches = products.filter((p) =>
+    p.title.toLowerCase().includes(query.toLowerCase())
+  );
+
+  // Returns markup with matching part of result underlined
+  const formatMatch = (match: string, query: string) => {
+    const parts = splitByQuery(match, query);
+
+    return (
+      <span className="capitalize text-xs">
+        {parts.before}
+        <b className="underline font-normal">{parts.query}</b>
+        {parts.after}
+      </span>
+    );
+  };
+
+  return (
+    <CommandList>
+      <CommandEmpty className="pl-4 py-2 text-sm italic">
+        No results found
+      </CommandEmpty>
+      <CommandGroup heading={`Products (${matches.length})`}>
+        {matches.map((match) => {
+          const url = `${routes.products.href}/${getSlugFromTitle(match.title)}`;
+
+          return (
+            <CommandItem key={match.id} className="!py-0">
+              <Button
+                asChild
+                variant="ghost"
+                className="w-full flex justify-between items-baseline"
+              >
+                <a href={url} className="">
+                  {formatMatch(match.title, query)}
+                  <span className="text-xs text-muted-foreground font-normal">
+                    {match.category.name.toLowerCase()}
+                  </span>
+                </a>
+              </Button>
+            </CommandItem>
+          );
+        })}
+      </CommandGroup>
+    </CommandList>
+  );
+}
