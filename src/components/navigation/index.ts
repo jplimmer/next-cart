22586e32173e68cea2011e15ddbf9ba32e168319
jplimmer <135324645+jplimmer@users.noreply.github.com>
2025-09-30 17:59:38 +0200

    Add SearchNavItem to barrel import

--- src/components/navigation/index.ts
+++ src/components/navigation/index.ts
@@ -1,4 +1,5 @@
 export { BasketNavItem } from './basket-nav-item';
 export { PagesNavMenu } from './pages-nav-menu';
+export { SearchNavItem } from './search-nav-item';
 export { UserNavItem } from './user-nav-item';
 export { UtilitiesNavMenu } from './utilities-nav-menu';
