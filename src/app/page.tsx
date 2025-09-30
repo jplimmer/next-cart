e09305fb646c8e71fe32991490f4f297fbcd2c91
Arif.A <arif.sain@gmail.com>
2025-09-30 14:56:13 +0200

    Wrap FeaturedProducts in Suspense for improved loading indication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import FeaturedProducts from '@/components/root-page/featured-products';
 import Hero from '@/components/root-page/hero';
+import { Suspense } from 'react';
 
 export default async function Home() {
   return (
     <main className="content-grid full-width">
       <Hero />
-      <FeaturedProducts />
+      <Suspense fallback={<div>Loading featured products...</div>}>
+        <FeaturedProducts />
+      </Suspense>
     </main>
   );
 }
