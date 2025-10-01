68d5a7b9e7d70060f87123bf0f63e6767da9bb7c
Arif.A <arif.sain@gmail.com>
2025-10-01 10:05:11 +0200

    Remove unused imports for LoadingSpinner and Suspense in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import FeaturedProducts from '@/components/root-page/featured-products';
 import Hero from '@/components/root-page/hero';
-import { LoadingSpinner } from '@/components/loading/loading-spinner';
-import { Suspense } from 'react';
 
 export default async function Home() {
   return (
