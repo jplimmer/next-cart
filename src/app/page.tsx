d2b27f4004673c70719372ef91e54e944ff68904
Arif.A <arif.sain@gmail.com>
2025-10-01 10:00:06 +0200

    Replace fallback div with LoadingSpinner in Suspense for FeaturedProducts and SearchBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import FeaturedProducts from '@/components/root-page/featured-products';
 import Hero from '@/components/root-page/hero';
+import { LoadingSpinner } from '@/components/loading/loading-spinner';
 import { Suspense } from 'react';
 
 export default async function Home() {
   return (
     <main className="content-grid full-width">
       <Hero />
-      <Suspense fallback={<div>Loading featured products...</div>}>
+      <Suspense
+        fallback={<LoadingSpinner text="Loading featured products..." />}
+      >
         <FeaturedProducts />
       </Suspense>
     </main>
